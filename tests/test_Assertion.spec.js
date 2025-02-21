cad7283282d8d736d10f19378fc9ddab52d6cd1b
Cathy <Cathy.hettison@gmail.com>
2025-02-21 14:51:46 -0800

    1st Commit

---
+++ tests/test_Assertion.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test'
+
+test('Login', async({ page })=>   {
+
+await page.goto('https://practicetestautomation.com/practice-test-login/')
+//await expect(page).toHaveTitle('Test login') 
+await expect(page.getByText('Test login')).toContainText('Test login')
+await page.click('id=username')
+await page.locator('id=username').fill('student')
+await page.click('id=password')
+await page.locator('id=password').fill('Password123')
+await page.locator('#submit').click()
+})
